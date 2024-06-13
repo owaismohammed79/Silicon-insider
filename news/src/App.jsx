8979421730cde178ed4cc96047d2441a00b39fef
bharth <bharthps821@gmail.com>
2024-06-13 23:40:33 +0530

    updated

---
+++ news/src/App.jsx
@@ -0,0 +1,15 @@
+import Body from '../pages/Body.jsx'
+import Sidebar from '../pages/Sidebar.jsx'
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <Body />
+    <Sidebar/>
+    </>  
+   
+  )
+}
+
+export default App
