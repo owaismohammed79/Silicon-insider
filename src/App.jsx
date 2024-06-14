2d322097d1f5b80c26dcf7b8a0ea723cd26c373e
Mohammed Owais <owaismohammed795@gmail.com>
2024-06-14 06:38:19 +0530

    Added footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Sidebar from '../pages/Sidebar.jsx'
 import LandingPg2 from '../src/pages/LandingPg2.jsx'
 import LandingPg3 from '../src/pages/LandingPg3.jsx'
 import LandingPg4 from '../src/pages/LandingPg4.jsx'
+import Footer from '../src/Components/Footer.jsx'
 
 function App() {
   return (
     <div >
-    <Sidebar/>
-    <Body />
-    <div>
-      <LandingPg2 />
-      <LandingPg3 />
-      <LandingPg4 />
-    </div>
-
+      <Sidebar/>
+      <Body />
+      <div>
+        <LandingPg2 />
+        <LandingPg3 />
+        <LandingPg4 />
+      </div>
+      <Footer />
     </div>  
    
   )
